1934ee26c23ea2c8091e5606d5971611520fa567
+ <+>
2023-03-24 15:44:29 +0300

    Assignment DOne

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,17 @@ class MoviesController < ApplicationController
     render json: movie
   end
 
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
+  end  #Explicitly Specifying a Serializer
+
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end #Explicitly Specifying a Serializer
+
   private
 
   def render_not_found_response
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :movies, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/movies/:id/summary', to: 'movies#summary' #Explicitly Specifying a Serializer
+  get '/movie_summaries', to: 'movies#summaries' #Explicitly Specifying a Serializer
 end
